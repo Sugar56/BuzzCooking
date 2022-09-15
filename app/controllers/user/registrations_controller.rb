263924bcb397d276c9b42ffe3f698cc294723618
Sugar56 <108616992+Sugar56@users.noreply.github.com>
2022-09-15 07:28:39 +0000

    [Add] 退会確認画面作成

--- app/controllers/user/registrations_controller.rb
+++ app/controllers/user/registrations_controller.rb
@@ -59,4 +59,11 @@ class User::RegistrationsController < Devise::RegistrationsController
   # def after_inactive_sign_up_path_for(resource)
   #   super(resource)
   # end
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:last_name, :first_name, :last_name_kana, :first_name_kana, :address, :telephone_number, :name, :self_introduction])
+  end
 end
--- app/controllers/user/users_controller.rb
+++ app/controllers/user/users_controller.rb
@@ -1,14 +1,37 @@
 class User::UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
-    @recipes = @user.recipes.page(params[:page])
-    @recipes_all = @user.recipes.all
+    @recipes = Recipe.page(params[:page])
+    @recipes_all = Recipe.all
   end
 
   def edit
     @user = User.find(params[:id])
   end
 
+  def update
+    @user = current_user
+    if @user.update(user_params)
+      redirect_to user_path(@user), notice: "会員情報を編集しました"
+    else
+      render "edit"
+    end
+  end
+
   def check
+  end
+
+  def withdraw
+    @user = current_user
+    @user.update(is_deleted: true)
+    reset_session
+    flash[:notice] = "退会処理を実行致しました"
+    redirect_to root_path
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:last_name, :first_name, :last_name_kana, :first_name_kana, :email, :address, :telephone_number, :name, :self_introduction, :is_deleted)
   end
 end

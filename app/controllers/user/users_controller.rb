02ff92bccc48ea104553098d3ae5948d899888c9
Sugar56 <108616992+Sugar56@users.noreply.github.com>
2022-09-15 06:27:53 +0000

    [Add] ユーザー詳細画面作成

--- app/controllers/user/users_controller.rb
+++ app/controllers/user/users_controller.rb
@@ -1,5 +1,8 @@
 class User::UsersController < ApplicationController
   def show
+    @user = User.find(params[:id])
+    @recipes = @user.recipes.page(params[:page])
+    @recipes_all = @user.recipes.all
   end
 
   def edit
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,17 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  has_one_attached :profile_image
+
+  def get_profile_image(width, height)
+    unless profile_image.attached?
+      file_path = Rails.root.join('app/assets/images/no_image.jpeg')
+      profile_image.attach(io: File.open(file_path), filename: 'default-image.jpg', content_type: 'image/jpeg')
+    end
+    profile_image.variant(resize_to_limit: [width, height]).processed
+  end
+
   def self.guest
     find_or_create_by!(name: 'guestuser' ,email: 'guest@example.com') do |user|
       user.password = SecureRandom.urlsafe_base64

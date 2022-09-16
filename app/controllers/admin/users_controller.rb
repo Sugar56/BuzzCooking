71ed5dbef3940a8fad4edf4a5b358356be695d75
Sugar56 <108616992+Sugar56@users.noreply.github.com>
2022-09-16 16:54:24 +0000

    [Add] admin側トップページ,ユーザー詳細・編集画面作成,管理者ログイン・ジャンル一覧・編集画面レイアウト修正

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,7 +1,25 @@
 class Admin::UsersController < ApplicationController
   def show
+    @user = User.find(params[:id])
+    @recipes = @user.recipes.all
   end
 
   def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = user.find(params[:id])
+    if @user.update(user_params)
+      redirect_to admin_user_path(@user.id), notice: "会員情報を編集しました"
+    else
+      render "edit"
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:last_name, :first_name, :last_name_kana, :first_name_kana, :email, :address, :telephone_number, :name, :self_introduction, :is_deleted)
   end
 end
